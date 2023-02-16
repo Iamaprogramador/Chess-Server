25e6e8b5a746394d6a1a4d8c92d69bf1b141c809
Iamaprogramador <104740094+Iamaprogramador@users.noreply.github.com>
2023-02-16 22:22:22 +0000

    initial

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/index.html');
 });
 
-app.get('/')
+app.get('/cod.js', (req, res) => {
+    res.sendFile(__dirname + '/client/script.js');
+});
+
+app.get('/style.css', (req, res) => {
+    res.sendFile(__dirname + '/client/script.js');
+});
